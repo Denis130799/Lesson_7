da93211d4bc40bc5bd78badc9d0b9a8b4b4a67a1
Denis <denismishchenko13@gmail.com>
2017-08-29 19:05:35 +0300

    Добавил проект

---
+++ Lesson_7/Lesson_7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson_7
+//
+//  Created by Denis on 29.08.17.
+//  Copyright © 2017 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson_7/Lesson_7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson_7
+//
+//  Created by Denis on 29.08.17.
+//  Copyright © 2017 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
