07fc285c65ccb755642e58f7c091690339e584b8
Denis <denismishchenko13@gmail.com>
2017-09-01 11:47:48 +0300

    Добавил проект

---
+++ Lesson_7/Protocols_And_Delegates_HW/Protocols_And_Delegates_HW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Protocols_And_Delegates_HW
+//
+//  Created by Denis on 01.09.17.
+//  Copyright © 2017 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
